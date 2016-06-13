b42efc067824140c8f798655e48a349e56e721f8
Sanket Patel <sanket0354@gmail.com>
2016-06-13 18:38:50 -0400

    Major Refactoring
    - changed the footer to a simpler and responsive one
    - changed header logo
    - changed all codes to php type files
    - added home page
    - added comments

---
+++ html/footer.php
@@ -0,0 +1,10 @@
+<!-- 
+	navbar-fixed-bottom : to fix footer to bottom
+-->
+<footer class="footer-distributed navbar-fixed-bottom">
+	<p>
+		Continuous learning is the key to success<br /> <b> World Mindz &copy;
+			2016<br /> All Rights Reserved
+		</b>
+	</p>
+</footer>
