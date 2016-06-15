39c05d9237b07148e898955d410e583b0fb18d22
Sanket Patel <sanket0354@gmail.com>
2016-06-15 19:54:48 -0400

    - Demo for side navigation bar
    - code needs to be cleaned and few things are needed to be added

---
+++ html/side_navigation_bar.php
@@ -0,0 +1,45 @@
+<html>
+<head>
+<title>Side Navigation Bar</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width = device-width, initial-scale = 1">
+<link rel="stylesheet"
+	href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+<script
+	src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
+<script
+	src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+
+<link rel="stylesheet" type="text/css"
+	href="../css/side_navigation_bar.css">
+</head>
+<body>
+	<nav class="navbar navbar-default" role="navigation">
+		<!-- Brand and toggle get grouped for better mobile display -->
+		<div class="navbar-header">
+			
+			<a class="navbar-brand" href="#">Brand</a>
+		</div>
+
+		<!-- Collect the nav links, forms, and other content for toggling -->
+		<div class="collapse navbar-collapse navbar-ex1-collapse">
+			<ul class="nav navbar-nav">
+				<!-- 
+				--If you want to set the link to active so that it highlights which link is active use class active
+				--commenting out for now ( for future use )
+				<li class="active"><a href="#">Link</a></li>
+				
+				 -->
+				<li ><a href="#">Link</a></li>
+				<li><a href="#">Link</a></li>
+				<li><a href="#">Link</a></li>
+				<li><a href="#">Link</a></li>
+				<li><a href="#">Link</a></li>
+				
+
+			</ul>
+		</div>
+		<!-- /.navbar-collapse -->
+	</nav>
+</body>
+</html>
